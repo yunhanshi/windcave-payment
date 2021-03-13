9c007843e497b7b2aff6b76e9be761106bd327ca
yunhan shi <jack.shi@nunu.com>
2021-03-13 17:18:45 +1300

    fix autoload

--- autoload.php
+++ autoload.php
@@ -1,8 +1,8 @@
 <?php
 function classLoader($class)
 {
-    $path = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-    $path = str_replace('WindcavePayment\\', '', $path);
+    $path = str_replace('WindcavePayment\\', '', $class);
+    $path = str_replace('\\', DIRECTORY_SEPARATOR, $path);
     $file = __DIR__ . DIRECTORY_SEPARATOR .'src'. DIRECTORY_SEPARATOR . $path . '.php';
     if (file_exists($file)) {
         require_once $file;
